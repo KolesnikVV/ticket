d17406fe203ea9b3a62b9d4e8e187ca8fc643868
KolesnikVV <Voltpluc@ya.ru>
2023-03-27 20:34:43 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+//- Задача
+//
+//        Необходимо реализовать следующий метод:
+//
+//        1. Получаем на входе массив чисел.
+//
+//        2. Все четные числа увеличиваем на единицу.
+//
+//        3. Возвращаем кусок списка с 3-го по 7-й элемент.
 public class Main {
+    static void calculateArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                int value = arr[i];
+                value += 1;
+                arr[i] = value;
+            }
+            if (i >= 2 && i <= 7) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        calculateArr(arr);
     }
 }
